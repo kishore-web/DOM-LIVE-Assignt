3ea691ca7df8b36c5f9d0d88b8deb6855cb28a94
Kishore Ch <kishu.559@gmail.com>
2022-11-30 22:01:57 +0530

    filter

--- index.js
+++ index.js
@@ -3,9 +3,12 @@ let inputVal = document.getElementById("url")
 let mainContainer = document.getElementById("main-container")
 let selectItems= document.getElementById("select-items")
 let warning = document.getElementById("warning")
+let filterList = document.querySelectorAll(".fil")
+let selectOptions = document.querySelector("#form>select")
 
 addButton.addEventListener("click", addImage);
-function addImage() {
+
+function addImage(e) {
   e.preventDefault();
   if(inputVal.value=="") {
     warning.innerText = "Please enter valid URL"
@@ -13,21 +16,20 @@ function addImage() {
       warning.innerText = ""
     }, 1000);
   } else {
-  const mainDiv = document.createElement("div")
-  const imgDiv = document.createElement("img")
-  const deleteButton = document.createElement("button")
-  imgDiv.setAttribute("src", inputVal.value)
-  
-  mainDiv.setAttribute("class","mainDiv")
-  imgDiv.setAttribute("class","img-pro")
+    const mainDiv = document.createElement("div")
+    const imgDiv = document.createElement("img")
+    const deleteButton = document.createElement("button")
+    imgDiv.setAttribute("src", inputVal.value)
+    mainDiv.setAttribute("class","mainDiv")
+    imgDiv.setAttribute("class","img-pro")
     deleteButton.innerText = "Delete"
     deleteButton.setAttribute("class", "del")
-  mainDiv.append(imgDiv,deleteButton)
-  
-  mainContainer.appendChild(mainDiv);
-  inputVal.value = ""
+    mainDiv.append(imgDiv,deleteButton)
+    mainContainer.appendChild(mainDiv);
+    inputVal.value = ""
   }
 }
+
 
 mainContainer.addEventListener("click", (e) => {
   if (e.target.classList.contains("del")) {
